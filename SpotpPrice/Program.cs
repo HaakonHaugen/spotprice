50e9b4cd89d651d8d95cabe05db5f971aa0f498e
HakonHaugen <Hakon.Haugen@coop.no>
2024-12-09 14:40:10 +0100

    removed commented  lines.

--- SpotpPrice/Program.cs
+++ SpotpPrice/Program.cs
@@ -8,12 +8,6 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-//builder.Services
-//    .AddApplicationInsightsTelemetryWorkerService()
-//    .ConfigureFunctionsApplicationInsights();
-
-
 builder.Services
       .AddApplicationInsightsTelemetryWorkerService()
       .ConfigureFunctionsApplicationInsights();
