efd800fc7c43c9186c6e89884f50e03e3200a3e7
HakonHaugen <Hakon.Haugen@coop.no>
2024-12-09 22:08:17 +0100

    adjustments

--- SpotpPrice/Function1.cs
+++ SpotpPrice/Function1.cs
@@ -7,19 +7,24 @@ namespace SpotpPrice
     public class Function1
     {
         private readonly ILogger _logger;
+        private readonly IStromPrisClient _stromPrisClient;
 
         public Function1(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<Function1>();
+            
         }
 
         [Function("Function1")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
 
-            
+            var result = await _stromPrisClient.GetNordPoolPrisAsync();
+            System.Console.WriteLine(result);
+
+
             if (myTimer.ScheduleStatus is not null)
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
